0433797addd3c5bf7bdb298e3bcebc81f9ec21e5
Ultra-Code <mega.alpha100@gmail.com>
2024-09-18 17:48:23 +0000

    Use threadpool to handle multiple request simultenously
    making handing request asynchronous

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 //! library implementation of `mgrep` and `guessing_game`
 #![deny(clippy::all)]
 #![warn(clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![feature(vec_push_within_capacity)]
 pub use mgrep::Case;
 pub mod guessing_game;
 pub mod mgrep;
+pub mod threadpool;
 pub mod webserver;
